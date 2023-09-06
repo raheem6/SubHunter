c51339c093e46a6985fb0386d404cbbd81ed7d12
raheem6 <raheemabuhumra@csus.edu>
2023-09-05 20:24:14 -0700

    commit

--- app/src/main/java/com/example/subhunter/MainActivity.java
+++ app/src/main/java/com/example/subhunter/MainActivity.java
@@ -31,6 +31,13 @@ public class MainActivity extends AppCompatActivity {
     int distanceFromSub;
     boolean debugging = true;
 
+    // Here are all the objects(instances)
+    // of classes that we need to do some drawing
+    ImageView gameView;
+    Bitmap blankBitmap;
+    Canvas canvas;
+    Paint paint;
+
     /*
         Android runs this code just before
         the player sees the app.
@@ -48,6 +55,26 @@ public class MainActivity extends AppCompatActivity {
         Point size = new Point();
         display.getSize(size);
 
+        // Initialize our size based variables
+        // based on the screen resolution
+        numberHorizontalPixels = size.x;
+
+        numberVerticalPixels = size.y;
+        blockSize = numberHorizontalPixels / gridWidth;
+        gridHeight = numberVerticalPixels / blockSize;
+        // Initialize all the objects ready for drawing
+        blankBitmap = Bitmap.createBitmap(numberHorizontalPixels,
+                numberVerticalPixels,
+                Bitmap.Config.ARGB_8888);
+
+        canvas = new Canvas(blankBitmap);
+        gameView = new ImageView(this);
+        paint = new Paint();
+
+        // Tell Android to set our drawing
+        // as the view for this app
+        setContentView(gameView);
+
         Log.d("Debugging", "In onCreate");
         newGame();
         draw();
@@ -59,6 +86,11 @@ public class MainActivity extends AppCompatActivity {
            and after the player wins a game.
     */
     public void newGame(){
+        Random random = new Random();
+        subHorizontalPosition = random.nextInt(gridWidth);
+        subVerticalPosition = random.nextInt(gridHeight);
+        shotsTaken = 0;
+
         Log.d("Debugging", "in newGame");
     }
 
@@ -67,8 +99,38 @@ public class MainActivity extends AppCompatActivity {
            The grid lines, the HUD and
            the touch indicator
     */
-    void draw(){
+    void draw() {
+        gameView.setImageBitmap(blankBitmap);
+
+        // Wipe the screen with a white color
+        canvas.drawColor(Color.argb(255, 255, 255, 255));
+
+        // Change the paint color to black
+        paint.setColor(Color.argb(255, 0, 0, 0));
+
+        // Draw the vertical lines of the grid
+        canvas.drawLine(blockSize * 1, 0,
+                blockSize * 1, numberVerticalPixels,
+                paint);
+
+        // Draw the horizontal lines of the grid
+        canvas.drawLine(0, blockSize * 1,
+                numberHorizontalPixels, blockSize * 1,
+                paint);
+
+        // Re-size the text appropriate for the
+        // score and distance text
+        paint.setTextSize(blockSize * 2);
+        paint.setColor(Color.argb(255, 0, 0, 255));
+        canvas.drawText(
+                "Shots Taken: " + shotsTaken +
+                        "  Distance: " + distanceFromSub,
+                blockSize, blockSize * 1.75f,
+                paint);
+
+
         Log.d("Debugging", "In draw");
+        printDebuggingText();
     }
 
     /*
@@ -100,7 +162,39 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // This code prints the debugging text
-    void printDebuggingText(){
+    public void printDebuggingText(){
+        paint.setTextSize(blockSize);
+        canvas.drawText("numberHorizontalPixels = "
+                        + numberHorizontalPixels,
+                50, blockSize * 3, paint);
+        canvas.drawText("numberVerticalPixels = "
+                        + numberVerticalPixels,
+                50, blockSize * 4, paint);
+        canvas.drawText("blockSize = " + blockSize,
+                50, blockSize * 5, paint);
+        canvas.drawText("gridWidth = " + gridWidth,
+                50, blockSize * 6, paint);
+        canvas.drawText("gridHeight = " + gridHeight,
+                50, blockSize * 7, paint);
+        canvas.drawText("horizontalTouched = " +
+                        horizontalTouched, 50,
+                blockSize * 8, paint);
+        canvas.drawText("verticalTouched = " +
+                        verticalTouched, 50,
+                blockSize * 9, paint);
+        canvas.drawText("subHorizontalPosition = " +
+                        subHorizontalPosition, 50,
+                blockSize * 10, paint);
+        canvas.drawText("subVerticalPosition = " +
+                        subVerticalPosition, 50,
+                blockSize * 11, paint);
+        canvas.drawText("hit = " + hit,
+                50, blockSize * 12, paint);
+        canvas.drawText("shotsTaken = " +
+                        shotsTaken,
+                50, blockSize * 13, paint);
+        canvas.drawText("debugging = " + debugging,
+                50, blockSize * 14, paint);
 
     }
 }
